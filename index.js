aa0694750bb27b9c02423ac669518db8f4c8cecd
tyang1 <teefyang7857@gmail.com>
2020-07-27 23:27:00 -0700

    adds env variable

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const port = 3001;
-// const path = require("path");
+
 
 app.use(express.static(__dirname));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+// class Random{
+
+//     static getUserId(e){
+//         return a.a(this,
+//             void 0,
+//             void 0,
+//             function*(){yield Object(T.c)(),Object(T.s)("getUserId",e);
+//                     const t=(yield I.a.getSubscription()).deviceId;
+//                     return Object(T.j)(e,t),t}
+//                     )}
+// }
